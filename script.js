2d22016d5794070dd61731cc0224b979d4fcd7d5
idillia <mila.antonova@gmail.com>
2015-12-07 09:09:20 -0800

    more changes

--- script.js
+++ script.js
@@ -15,11 +15,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
       });
     }
   }
-  
   resize();
 
   angular.module('bridgeApp', [])
-  .controller('cardsCtrl', ['$scope', function($scope){
+  .directive('btnAttrClick', ['gameState', function(gameState){
+    return {
+      link: function(scope, element, attr) {
+        element.on('click', function(){
+          gameState.trumpSuit = attr.class;
+        })
+      }
+    }
+  }])
+
+  .factory('gameState', function(){
+    return {
+      trumpSuit: null
+    }
+  })
+  .controller('cardsCtrl', ['$scope', 'gameState', function($scope, gameState){
+    console.log(gameState);
     var zIdx;
     var east = [], west= [], south = [], north =[];  
     var Card = function(value, name, suit, zIndex) {
@@ -46,8 +61,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         return bValue - aValue;
       });
     };
-
-    
 
     var suffleDeck = function(){
       this.ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"];
@@ -78,6 +91,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         north.cards = this.cards.slice(39,52);
         north.sort();
       }
+      console.log(north);
       return {east, west, south, north};
     }
     $scope.deck = suffleDeck();
@@ -86,6 +100,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $scope.selectCardNorth = function(aCard) {
       $scope.chosenCardNorth = $scope.deck.north.cards.splice(aCard,1); 
       $scope.chosenCardNorth[0].zIndex = zIdx++;
+      console.log($scope.chosenCardNorth);
     }
 
     $scope.selectCardEast = function(aCard) {
@@ -106,44 +121,62 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var winningTrick = function(trump) {
       var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"];
       var suits = ["diamonds", "clubs", "hearts", "spades"];
-      var trickHolder =[];
-      console.log("running");
-      console.log(ranks.indexOf($scope.chosenCardNorth[0].name));
-      if (($scope.chosenCardNorth[0].suit == $scope.chosenCardWest[0].suit) && ($scope.chosenCardNorth[0].suit == $scope.chosenCardEast[0].suit) && ($scope.chosenCardNorth[0].suit == $scope.chosenCardSouth[0].suit)) {
-        trickHolder.push.call(ranks.indexOf($scope.chosenCardNorth[0].name), ranks.indexOf($scope.chosenCardWest[0].name), ranks.indexOf($scope.chosenCardEast[0].name), ranks.indexOf($scope.chosenCardSouth[0].name)) 
-        
-        console.log(trickHolder);
+      console.log($scope.chosenCardNorth);
+      var n = $scope.chosenCardNorth[0];
+      var e = $scope.chosenCardEast[0];
+      var w = $scope.chosenCardWest[0];
+      var s = $scope.chosenCardSouth[0];
+      console.log(n.suit, e.name, w, s);
+      var winner;
+      if (((n.suit == w.suit) && (n.suit == e.suit)) && (n.suit == s.suit)) {
+        console.log('all one suit');
+        if (ranks.indexOf(n.name) > ranks.indexOf(e.name) && ranks.indexOf(n.name) > ranks.indexOf(w.name) && ranks.indexOf(n.name) > ranks.indexOf(s.name)) {
+          winner = 'nW';
+        } else if (ranks.indexOf(s.name) > ranks.indexOf(n.name) && ranks.indexOf(s.name) > ranks.indexOf(w.name) && ranks.indexOf(s.name) > ranks.indexOf(e.name)) {
+          winner = 'sW';
+        } 
+        else if (ranks.indexOf(w.name) > ranks.indexOf(n.name) && ranks.indexOf(w.name) > ranks.indexOf(s.name) && ranks.indexOf(w.name) > ranks.indexOf(e.name)) {
+          winner = 'wW';
+        } else {
+          winner = 'eW';
+        }
       }
+      console.log(gameState);
+      console.log(winner);
+      return winner;
     } 
 
-
-
+    $scope.score = {ns: 0, ew: 0};
     $scope.takeTrick = function() {
-      winningTrick();
-
-      console.log("trick taken");
-
+      var winningPair = winningTrick();
+      if (winningPair == 'nW' || winningPair == 'sW') {
+         $scope.score.ns++;
+      } else {
+         $scope.score.ew++;
+      }
       $scope.chosenCardNorth = '';
       $scope.chosenCardEast = '';
       $scope.chosenCardWest = '';
       $scope.chosenCardSouth = '';
-
-    }
-    
-
+    }     
   }])
   .directive('myCard', function(){
     return {
       template: '<img src="img/cards/{{card.value}}.svg" style="width:100px; z-index: {{card.zIndex}}; position: relative; left:30px;" ng-repeat="card in chosenCardEast"><img src="img/cards/{{card.value}}.svg" style=" top: -30px; width:100px; z-index: {{card.zIndex}}; position: relative; left:-40px;" ng-repeat="card in chosenCardNorth"><img src="img/cards/{{card.value}}.svg" style="width:100px; z-index: {{card.zIndex}}; position: relative; top: -10px; left:-95px;" ng-repeat="card in chosenCardWest"><img src="img/cards/{{card.value}}.svg" style="width:100px; z-index: {{card.zIndex}}; position: relative; top: 40px; left:-250px;" ng-repeat="card in chosenCardSouth">'
     }
   })
-  .directive('score', function() {
+  .directive('score', function($compile) {
+    var linker = function(scope, element, attr){
+    }
     return {
-      template: 'North-South: 0, East-West: 0'
+      restrict: "E",
+      link: linker,
+      scope: {
+        content: '=info'
+      },
+      template: '<div>North-South: {{content.ns}}, East-West: {{content.ew}}'
     }
   })
-
-  
 });
 
 
